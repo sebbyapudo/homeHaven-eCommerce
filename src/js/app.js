1f7fc8df5aa1064fa075cca2ff0c831e92d5d2f6
sebbyapudo <sebbyapudo@gmail.com>
2023-07-13 01:08:08 +0300

    Declared all the global variables and set up the structure for classes that performs different tasks

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
 // homeHave e-commerce
+// Declaring global variables
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDom = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+
