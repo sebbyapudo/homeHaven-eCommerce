3bb8663d945828727be5c75f8a22e47e758bbdc5
sebbyapudo <sebbyapudo@gmail.com>
2023-07-14 23:55:41 +0300

    Dynamically rendered markup in the products section

--- src/js/app.js
+++ src/js/app.js
@@ -36,7 +36,27 @@ class Products {
 
 // display products
 class UI {
-
+  displayProducts(products){
+  let result = ''
+  products.forEach(product => {
+    result += `
+    <!-- single product -->
+        <article class="product">
+          <div class="img-container">
+            <img src=${product.image} alt="product" class="product-img">
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+    <!-- end of single product -->
+    `
+  })
+  productsDOM.innerHTML = result;
+  }
 }
 
 // local storage
@@ -51,6 +71,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const products = new Products();
 
   // get all products 
-  products.getProducts().then(products => console.log(products));
+  products.getProducts().then(products => ui.displayProducts(products));
 })
 
