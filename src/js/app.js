731c0475cf12f6b823b23d0318430adb626c0e7a
sebbyapudo <sebbyapudo@gmail.com>
2023-07-15 14:08:30 +0300

    Implemented the add to cart button functionality

--- src/js/app.js
+++ src/js/app.js
@@ -58,6 +58,23 @@ class UI {
   })
   productsDOM.innerHTML = result;
   }
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll('.bag-btn')];
+    buttons.forEach((button) => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+      if(inCart){
+        button.innerText = "In Cart"
+        button.disabled = true;
+      }
+      else{
+        button.addEventListener('click', (event) => {
+          event.target.innerText = "In bag"
+          event.target.disabled = true
+        })
+      }
+    })
+  }
 }
 
 // local storage
@@ -77,6 +94,8 @@ document.addEventListener('DOMContentLoaded', () => {
   products.getProducts().then(products => {
   ui.displayProducts(products);
   Storage.saveProducts(products);
+  }).then(() => {
+    ui.getBagButtons();
   });
 })
 
